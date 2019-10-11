077d22bf6b4a4164da4f5aaefcca66715f223796
roopabdr <roopabdr@gmail.com>
2019-10-11 16:28:49 +0530

    added OR condition to origin

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 let whitelist = ['https://roopabdr.github.io', 'http://localhost:3000','https://gt-enrich-server.herokuapp.com','https://gt-enrich.herokuapp.com']
 let corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
