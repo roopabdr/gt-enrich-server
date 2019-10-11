ee4b61a2d24defa4a2b9e963a456af78b68cc3cf
roopabdr <roopabdr@gmail.com>
2019-10-11 14:29:15 +0530

    made changes to how the data is read and converted to json

--- upload.js
+++ upload.js
@@ -6,17 +6,22 @@ module.exports = function upload(req, res) {
     // console.log('something',req.body.csvdata);
     
     let t=0;
-    // fileData = 'a,b,c,1,2,3,4,5,6';    
-    fileData = fileData.replace(/,/g, function (match) {
-        t++;
-        return (t % 24 === 0) ? "\n" : match;
-      });
+    // fileData = 'a,b,c<br>1,2,3<br>4,5,6';
+    fileData = fileData.replace(/<br>/g,'\n');
+    // fileData = fileData.replace(/^/g, function (match) {
+    //     t++;
+    //     return (t % 3 === 0) ? "\n" : match;
+    //   });
     // fileData = 'a,b,c\n1,2,3\n4,5,6';
     console.log(fileData);
     csv()
     .fromString(fileData)
     .then((json)=>{
         console.log('json inside',json); 
+        res.status(200).json(JSON.stringify(json[15]['Timekeeper Name']));
     });
-    res.send('Stringified');
+    // res.send('Stringified', JSON.stringify(jsonData[0]));
+    // console.log(jsonData[0]);
+    // res.status(200).json(JSON.stringify(jsonData[0].Date));
+    // res.send('Sent');
 }
