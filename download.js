a9a4dd116e6b6138f33eae872515f6348c78f8e3
roopabdr <roopabdr@gmail.com>
2019-10-15 12:43:03 +0530

    changed file save to FileSaver module

--- download.js
+++ download.js
@@ -1,4 +1,6 @@
 const XLSX = require('xlsx');
+const FileSaver = require('file-saver');
+const Blob = require('blob');
 // const LocalStorage = require('node-localstorage').LocalStorage;
 
 module.exports = function download(req, res) {
@@ -14,10 +16,23 @@ module.exports = function download(req, res) {
 
     console.log('Here we go.....', 2);
 
-    res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename=' + 'test.xlx');
-    return XLSX.writeFile(wb, 'file:///C:/Users/us53807/Documents/Roopa/test.xlsx');
+    // res.setHeader('Content-Type', 'application/octet-stream');
+    // res.setHeader('Content-Disposition', 'attachment; filename=' + 'test.xlx');
+    // XLSX.writeFile(wb, 'file:///C:/Users/us53807/Documents/Roopa/test.xlsx');
+
+    const wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
+
+    FileSaver.saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx');
 
     // console.log('Others', localStorage.getItem("TimeSheetDataKey"));
-    // res.send("Hellloooo");
+    res.send("Hellloooo");
+}
+
+const s2ab = (s) => {
+    let buf = new ArrayBuffer(s.length);
+    let view = new Uint8Array(buf);
+    for(let i=0; i<s.length; i++){
+        view[i] = s.charCodeAt(i) & 0xFF;
+    }
+    return buf;
 }
