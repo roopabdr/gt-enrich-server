dd61aa65a61d3ee17be601871831c3f429833737
roopabdr <roopabdr@gmail.com>
2019-10-15 12:36:21 +0530

    changed to returning the XLSX object

--- download.js
+++ download.js
@@ -16,8 +16,8 @@ module.exports = function download(req, res) {
 
     res.setHeader('Content-Type', 'application/octet-stream');
     res.setHeader('Content-Disposition', 'attachment; filename=' + 'test.xlx');
-    XLSX.writeFile(wb, 'file:///C:/Users/us53807/Documents/Roopa/test.xlsx');
+    return XLSX.writeFile(wb, 'file:///C:/Users/us53807/Documents/Roopa/test.xlsx');
 
     // console.log('Others', localStorage.getItem("TimeSheetDataKey"));
-    res.send("Hellloooo");
+    // res.send("Hellloooo");
 }
